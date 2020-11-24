16fbf7a9ae2611bffc0a86e81338b1ad27a8734b
kw <kacp.woja@gmail.com>
2020-11-24 22:22:32 +0100

    minor changes

--- wojakowski_jasina.sql
+++ wojakowski_jasina.sql
@@ -34,7 +34,6 @@ CREATE TABLE Orders (
     date_placed DATE NOT NULL,
     price FLOAT NOT NULL,
     status VARCHAR(50) NOT NULL,
-    delivery_id INT,
     CONSTRAINT FK_CustomerOrder
         FOREIGN KEY (customer_email)
         REFERENCES Customers(email_address)
@@ -51,44 +50,48 @@ CREATE TABLE Order_Products (
         REFERENCES Products(product_id)
 );
 
-CREATE TABLE Delivery_suppliers (
+CREATE TABLE Delivery_Suppliers (
     supplier_name VARCHAR(50) NOT NULL PRIMARY KEY,
     type VARCHAR(50) NOT NULL,
-    price FLOAT NOT NULL, 
-	tracking_url VARCHAR(200)
+    price FLOAT NOT NULL,
+    tracking_url VARCHAR(200)
 );
 
 CREATE TABLE Deliveries (
-    delivery_id INT NOT NULL PRIMARY KEY,
+    order_id INT NOT NULL PRIMARY KEY,
     customer_email VARCHAR(50) NOT NULL,
-    state VARCHAR(50) NOT NULL, 
-	supplier VARCHAR(50) NOT NULL, 
-	different_address NUMBER(1,0) DEFAULT 0, 
-	address VARCHAR(50), 
-	address_city VARCHAR(50), 
-	address_zipcode CHAR(6), 
-	date_sent DATE, 
-	date_delivered DATE, 
-	tracking_id VARCHAR(50), 
-	parcel_locker_id VARCHAR(50),
-    CONSTRAINT FK_CustomerDelivery
+    state VARCHAR(50) NOT NULL,
+    supplier VARCHAR(50) NOT NULL,
+    different_address NUMBER(1,0) DEFAULT 0,
+    address VARCHAR(50),
+    address_city VARCHAR(50),
+    address_zipcode CHAR(6),
+    date_sent DATE,
+    date_delivered DATE,
+    tracking_id VARCHAR(50),
+    parcel_locker_id VARCHAR(50),
+    CONSTRAINT FK_DeliveryOrder
+        FOREIGN KEY (order_id)
+        REFERENCES Orders(order_id),
+    CONSTRAINT FK_DeliveryCustomer
         FOREIGN KEY (customer_email)
         REFERENCES Customers(email_address),
-    CONSTRAINT FK_SupplierDelivery
+    CONSTRAINT FK_DeliverySupplier
         FOREIGN KEY (supplier)
-        REFERENCES Delivery_suppliers(supplier_name)
+        REFERENCES Delivery_Suppliers(supplier_name)
 );
 
 CREATE TABLE Reviews (
     review_id INT NOT NULL PRIMARY KEY,
     product_id INT NOT NULL,
-    customer_email VARCHAR(50) NOT NULL, 
-	score INT NOT NULL,
+    customer_email VARCHAR(50) NOT NULL,
+    score INT NOT NULL,
     text VARCHAR(500),
-    CONSTRAINT FK_ReviewProduct
+    CONSTRAINT FK_CustomerReview
         FOREIGN KEY (product_id)
         REFERENCES Products(product_id),
-    CONSTRAINT FK_CustomerReview
-        FOREIGN KEY (customer_email)
-        REFERENCES Customers(email_address)
+    CONSTRAINT FK_ProductReview
+         FOREIGN KEY (customer_email)
+         REFERENCES Customers(email_address)
 );
+
